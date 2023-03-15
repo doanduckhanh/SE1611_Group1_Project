71f688a9369399d046a5f2353f41a8696ba24a81
Nguyễn Tiến Nhất <nhatnthe153132@fpt.edu.vn>
2023-03-15 15:50:21 +0700

    update

---
+++ SE1611_Group1_Project/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SE1611_Group1_Project.Pages.Login
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
